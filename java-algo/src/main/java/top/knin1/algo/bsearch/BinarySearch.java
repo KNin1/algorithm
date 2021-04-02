ae8d7bec60e76f5e765e1c4d7c418569be2cc850
KNin1 <wangcpps@gmail.com>
2021-04-02 12:34:51 +0800

    Add binary search variant algorithm

--- java-algo/src/main/java/top/knin1/algo/bsearch/BinarySearch.java
+++ java-algo/src/main/java/top/knin1/algo/bsearch/BinarySearch.java
@@ -25,6 +25,94 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int searchFirstEqualElement(int[] arr, int value) {
+        if (isEmptyArray(arr)) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] < value) {
+                low = mid + 1;
+            } else if (arr[mid] > value) {
+                high = mid - 1;
+            } else {
+                if (mid == 0 || arr[mid - 1] != value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int searchLastEqualElement(int[] arr, int value) {
+        if (isEmptyArray(arr)) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] < value) {
+                low = mid + 1;
+            } else if (arr[mid] > value) {
+                high = mid - 1;
+            } else {
+                if (mid == arr.length - 1 || arr[mid + 1] != value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int searchFirstGreaterOrEqualElement(int[] arr, int value) {
+        if (isEmptyArray(arr)) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] >= value) {
+                if (mid == 0 || arr[mid - 1] < value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int searchLastLessOrEqualElement(int[] arr, int value) {
+        if (isEmptyArray(arr)) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] <= value) {
+                if (mid == arr.length - 1 || arr[mid + 1] > value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                high= mid - 1;
+            }
+        }
+        return -1;
+    }
+
     private static boolean isEmptyArray(int[] arr) {
         return arr == null || arr.length == 0;
     }
--- java-algo/src/test/java/top/knin1/algo/bsearch/BinarySearchTest.java
+++ java-algo/src/test/java/top/knin1/algo/bsearch/BinarySearchTest.java
@@ -12,4 +12,38 @@ public class BinarySearchTest {
         int index = BinarySearch.search(arr, 100);
         Assert.assertEquals(3, index);
     }
+
+    @Test
+    public void testSearchFirstEqualElement() {
+        int[] arr = {10, 90, 2, 100, 100, 82, 345, 100};
+        InsertSort.sort(arr, arr.length);
+        // {2, 10, 82, 90, 100, 100, 100, 345}
+        int index = BinarySearch.searchFirstEqualElement(arr, 100);
+        Assert.assertEquals(4, index);
+    }
+
+    @Test
+    public void testSearchLastEqualElement() {
+        int[] arr = {10, 90, 2, 100, 100, 82, 345, 100};
+        InsertSort.sort(arr, arr.length);
+        int index = BinarySearch.searchLastEqualElement(arr, 100);
+        Assert.assertEquals(6, index);
+    }
+
+    @Test
+    public void testsearchFirstGreaterOrEqualElement() {
+        int[] arr = {10, 90, 2, 100, 100, 82, 345, 100};
+        InsertSort.sort(arr, arr.length);
+        int index = BinarySearch.searchFirstGreaterOrEqualElement(arr, 100);
+        Assert.assertEquals(4, index);
+    }
+
+    @Test
+    public void testsearchLastLessOrEqualElement() {
+        int[] arr = {10, 90, 2, 100, 100, 82, 345, 100};
+        InsertSort.sort(arr, arr.length);
+        int index = BinarySearch.searchLastLessOrEqualElement(arr, 100);
+        Assert.assertEquals(6, index);
+    }
+
 }
